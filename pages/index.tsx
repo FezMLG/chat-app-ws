f6ae5693502a304cbbf4852c7d78c9ebc7aac194
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-03-30 11:59:45 +0200

    stylig and refactoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,45 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Home: NextPage = () => {
   const [message, addMessage] = useState<string>();
   const [messages, addMessages] = useState<string[]>([])
-  const myRef = useRef(null)
+  const scrollTo = useRef<HTMLDivElement>(null)
 
   const handleSending = () => {
-    console.log("message send");
     if (message != null) {
-      addMessages(prevState => {
-        return [
-          ...prevState,
-          message
-        ]
-      })
-      addMessage("")
+      addMessageToList(message);
     }
-    // @ts-ignore
-    myRef.current.scrollIntoView()
     console.log(messages);
   }
 
-  // const executeScroll = () => myRef.current.scrollIntoView()
+  const addMessageToList = (mess: string) => {
+    mess.trim();
+    addMessages(prevState => {
+      return [
+        ...prevState,
+        mess
+      ]
+    });
+    addMessage("")
+  }
 
-  function handleInput(e: any) {
+  useEffect(()=>{
+    setTimeout(()=>{
+      addMessageToList("3sek")
+    }, 3000)
+  }, [])
+
+  useEffect(()=>{
+    scrollTo.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest"
+    })
+  }, [messages])
+
+  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
     console.log(e.target.value);
     addMessage(e.target.value);
   }
@@ -39,17 +52,29 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div id="window" className={"w-screen h-screen bg-slate-100"}>
-      <div id="chat-window" className={"w-screen h-screen grid"}>
-        <div id="messages-container">
+    <div id="window" className={"w-screen h-full min-h-screen bg-slate-100"}>
+      <div id="chat-window" className={"max-w-xl mx-auto gap-5"}>
+        <div
+          id="messages-container"
+          className={"w-full h-full h-96 max-h-96 rounded-md border-2 overflow-scroll px-5 py-2"}
+          onClick={() => addMessageToList("mess")}
+        >
           {messages?.map((value, index)=>{
             return(
               <div key={index}>{value}</div>
             )
           })}
+          <div ref={scrollTo}/>
         </div>
-        <div id="input-container" className={""}>
-          <input type={"text"} id={"user-message"} onChange={handleInput} onKeyPress={handleKeyPress} value={message} ref={myRef}/>
+        <div id="input-container" className={"flex flex-row sticky bottom-0 w-full gap-5"}>
+          <input
+            type={"text"}
+            id={"user-message"}
+            className={"h-full w-full rounded-md border-2 px-2"}
+            onChange={handleInput}
+            onKeyPress={handleKeyPress}
+            value={message} autoComplete="off"
+          />
           <button onClick={handleSending}>Send</button>
         </div>
       </div>
